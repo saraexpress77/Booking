8c4df77fc5dce51fb3f3d0b87da0eb9a29223812
saraexpress77 <135681478+saraexpress77@users.noreply.github.com>
2023-06-07 14:56:44 +0200

    Final version
    Finished CRUD with extended search

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require("mongoose");
-
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 const Book = require('./models/books');
 
-
 mongoose.set('strictQuery', false);
+
+//add this to parse forms in x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+
 const connectDB = async () => {
     try {
          const conn = await mongoose.connect(process.env.MONGO_CONNECTION);
@@ -21,22 +27,25 @@ const connectDB = async () => {
 }
 
 app.get('/', (req, res) =>{
-    res.send({title:'Books'});
+    res.send("Welcome to the simple express Books API!");
 });
 
-app.get('/add-note', async(req, res) => {
+app.get('/books/generate', async(req, res) => {
     try{
         await Book.insertMany([
             {
-                title: "Sons",
+                title: "Sons of fire",
                 body: "Hi this body",
+                category: "Sci-fi"
             },
             {
-                title: "Daughters",
-                body: "Hi this yo",
+                title: "Dungeons",
+                body: "Hi this yo brotha",
+                category: "Drama"
             }
         ]);
-        res.send("Data sent.");
+        res.status(201)
+        res.send("Data successfully generated.");
     } catch (error) {
         console.log("err", + error);
     }
@@ -46,35 +55,96 @@ app.get('/add-note', async(req, res) => {
 app.get('/books', async(req, res) =>{
 
     const bookTitle = req.query.title;
+    const bookCategory = req.query.category;
 
-    if(bookTitle){
-        const book = await Book.findOne({title: bookTitle}).exec();
-        if(book){
+    if(bookCategory){
+        const books = await Book.find({category: bookCategory}).exec();
+
+        if(books.length !== 0){
             res.status(200);
-            res.json(book);
+            res.json(books);
+            return;
         }else{
             res.status(404);
-            res.send("We could not find the file");
+            res.send("We could not find a match!");
+            return;
+        }
+    }
+
+    if(bookTitle){
+        const book = await Book.find({title: bookTitle}).exec();
+        if(book.length !== 0){
+            res.status(200);
+            res.json(book);
+            return;
+        }else{
+            res.status(404);
+            res.send("We could not find a match!");
+            return;
         }
     } else{
         const book = await Book.find();
 
-        if(book){
+        if(book.length !== 0){
             res.status(200);
-            res.json(book)
+            res.json(book);
+            return;
         } else {
-            res.status(404)
+            res.status(404);
             res.send("Something went wrong. No books were found!");
+            return;
         }
     }
-
-
-
-
 });
+
+app.post('/books/add', async function (req, res) {
+    //Get the parsed information
+
+    const  bookObject = req.body;
+
+    if(bookObject.title == null || bookObject.body == null){
+        res.status(400);
+        res.send("Malformed book entry. Please provide title and body.")
+    }
+
+    await Book.create(bookObject).then((book) =>
+        res.json(book)
+    ).catch((err) => {
+        console.log(err.message);
+    });
+});
+
+//This is can be seen as PATCH too, not necessary since findByIdAndUpdate accepts partial updates.
+app.put('/books/:id',  async (req, res) => {
+    const book = await Book.findById(req.params.id);
+    if(!book){
+        res.status(404);
+        res.send("Book does not exist.");
+    }
+
+    await Book.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.status(200);
+            res.send("Book successfully updated!");
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
+});
+
+app.delete('/books/:id', async function (req, res) {
+
+    await Book.findByIdAndRemove(req.params.id).then(() => {
+        res.status(200);
+        res.send("Entry successfully deleted!");
+    }).catch((err) => {
+        console.log(err.message);
+    })
+})
+
 
 connectDB().then(() => {
     app.listen(PORT, () =>{
-        console.log(`Listerning on port ${PORT}`);
+        console.log(`Listening on port ${PORT}`);
     })
 });
